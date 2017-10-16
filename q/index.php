599475e6a87e78030bb65834fc1147eb3c4deaa9
Louis Birla <32454680+nilaeus@users.noreply.github.com>
2017-10-16 16:31:12 -0500

    Debugging #1

--- q/index.php
+++ q/index.php
@@ -1,7 +1,8 @@
 <?php
-  $q = $_GET["q"];
+
+$q = $_GET["q"];
   $go = [];
-  if ($q == "school"){
+  /*if ($q == "school"){
     array_push($go, "https://laketravis.schoology.com/login/ldap?school=17707999");
   } elseif ($q == "") {
     array_push($go, "https://aeio.io");
@@ -9,5 +10,5 @@
     array_push($go, "https://google.com/search??hl=en&btnI=I&q=" . str_replace("%20", "+", urlencode($_GET["q"])));
   }
   $go = json_encode($go);
-  echo '{"go":' . $go . '}';
+  echo '{"go":' . $go . '}';*/
 ?>
