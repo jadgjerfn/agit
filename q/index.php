936ad7b555b2311663affbd3ec47a06d79a57b8d
Louis Birla <32454680+nilaeus@users.noreply.github.com>
2017-10-16 16:24:24 -0500

    Fixed obvious problems

--- q/index.php
+++ q/index.php
@@ -1,4 +1,6 @@
 <?php
+  $q = $_GET["q"];
+  $go = [];
   if ($q == "school"){
     array_push($text, 'n{"b":"Redirecting you to HBMS Schoology...", "t":"Redirecting..", "img":["https://p4.zdassets.com/hc/theme_assets/958824/200230227/contact.png"], "clr":"darkblue"}');
     array_push($go, "https://laketravis.schoology.com/login/ldap?school=17707999");
@@ -7,4 +9,6 @@
   } else {
     array_push($go, "https://google.com/search??hl=en&btnI=I&q=" . str_replace("%20", "+", urlencode($_GET["q"])));
   }
+  $go = json_encode($go);
+  echo '{"go":' . $go . '}';
 ?>
