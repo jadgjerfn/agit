c7d99e937428543ce3dd2269832363254df153b4
Arkagaen Saiik <arkagaen@gmail.com>
2017-10-16 12:16:28 -0600

    Added index and gitignore

---
+++ index.php
@@ -0,0 +1,5 @@
+Color is undergoing massive changes.
+<Br>
+Color is for everyone, by everyone.
+<Br>
+Going open source and available is a big step. To contribute, visit our GitHub repo at <a href="https://GitHub.com/aeioio/agit">https://GitHub.com/aeioio/agit</a>
